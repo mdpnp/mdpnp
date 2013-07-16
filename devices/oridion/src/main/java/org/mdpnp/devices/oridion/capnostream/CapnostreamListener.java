7dcc446ee62df4d8b93fd78d29a5d6a8aa9377c4
jeffplourde <jeff@mdpnp.org>
2013-07-16 17:29:12 -0400

    sendConfigurableSetup was not sending the SetupItem. Parses the device identity.  Includes protocol revision.  DemoCapnostream20 also now sends LinkIsActive which should put ICE on the device display ... and alarm when the connection is lost.

--- devices/oridion/src/main/java/org/mdpnp/devices/oridion/capnostream/CapnostreamListener.java
+++ devices/oridion/src/main/java/org/mdpnp/devices/oridion/capnostream/CapnostreamListener.java
@@ -1,9 +1,13 @@
 package org.mdpnp.devices.oridion.capnostream;
 
+import java.util.Date;
+
 import org.mdpnp.devices.oridion.capnostream.Capnostream.CO2Units;
+import org.mdpnp.devices.oridion.capnostream.Capnostream.PulseOximetry;
 
 public interface CapnostreamListener {
-	void deviceIdSoftwareVersion(String s);
+	void deviceIdSoftwareVersion(String softwareMajorVersion, String softwareMinorVersion, 
+            Date softwareReleaseDate, PulseOximetry pulseOximetry, String revision, String number);
 	void numerics(long date, int etCO2, int FiCO2,
             int respiratoryRate, int spo2, int pulserate, int slowStatus,
             int CO2ActiveAlarms, int SpO2, int extendedCO2Status, int etCo2AlarmHigh, int etCo2AlarmLow, int rrAlarmHigh, int rrAlarmLow, int fico2AlarmHigh, int spo2AlarmHigh, int spo2AlarmLow, int pulseAlarmHigh, int pulseAlarmLow, CO2Units units, int extendedCO2Status2);
