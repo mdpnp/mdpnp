e0586e9608c6fe3d67ae1180eba98e796d59a6c7
dalonso <diego@mdpnp.org>
2013-07-23 12:26:34 -0400

    Improved searches depending on the role.

---
+++ clinical-scenarios/src/main/java/org/mdpnp/clinicalscenarios/client/user/comparator/UserComparator.java
@@ -0,0 +1,106 @@
+package org.mdpnp.clinicalscenarios.client.user.comparator;
+
+import java.lang.reflect.Method;
+import java.util.Comparator;
+
+import org.mdpnp.clinicalscenarios.client.user.UserInfoProxy;
+
+/**
+ * Basic comparator for users <p>
+ * @author diego@mdpnp.org <p>
+ *
+ */
+public class UserComparator implements Comparator<UserInfoProxy> {
+
+	private String userProperty;//property name
+	private boolean reverseOrder;
+	
+	
+	
+	public UserComparator(String prop){
+		this.userProperty = prop;
+		reverseOrder = false;
+	}
+	
+	public UserComparator(String prop, boolean b){
+		this.userProperty = prop;
+		reverseOrder = b;
+	}
+	
+	//getter and setter
+	public String getPersonProperty() {
+		return userProperty;
+	}
+
+	public void setPersonProperty(String personProperty) {
+		this.userProperty = personProperty;
+	}
+	
+	public void switcReverseOrder(){
+		reverseOrder = ! reverseOrder;
+	}
+	
+	
+	
+	/**
+	 * Compares two users based on the member variables of the class.
+	 * If the property provided is not in the UserInfoProxy class
+	 * it will return 0 and won´t establish and other.
+	 *  String type properties will be sorted lexicografically
+	 */
+		public int compare(UserInfoProxy user1, UserInfoProxy user2) {
+			//XXX CAREFUL!! Check the property is NOT NULL
+		      if(reverseOrder)
+		    	  return -1*( user1.getEmail().compareToIgnoreCase(user2.getEmail()) );  
+		      else
+		    	  return user1.getEmail().compareToIgnoreCase(user2.getEmail()) ; 
+		      
+//		      diego@mdpnp.org
+//		      I don't really want to, but if reflexion doesn't work we are going to need to add
+//		      one method/property for each property in the UserInfoProxy class, and maintain that when ned properties are added
+		      
+//			 Class<?> myclass = user1.getClass();  
+//			 String getter = "get" + Character.toUpperCase(this.userProperty.charAt(0)) + userProperty.substring(1);
+//			 try {  
+//				 Method getPropiedad = myclass.getMethod(getter);  
+//			       
+//			     Object property1 = getPropiedad.invoke(user1);  
+//			     Object property2 = getPropiedad.invoke(user2);  
+//			     			     
+//			     if(property1 instanceof Comparable && property2 instanceof Comparable) {  
+//			      Comparable prop1 = (Comparable)property1;  
+//			      Comparable prop2 = (Comparable)property2;  
+//			      if(reverseOrder)
+//			    	  return -1*( prop1.compareTo(prop2) );  
+//			      else
+//			    	  return prop1.compareTo(prop2) ;  
+//			     } 
+//			     else { //IF THEY ARE NOT COMPARABLE 
+//			    	 return 0;//doesn´t compare /sort		  
+//			     }  		    
+//			    }  
+//			    catch(Exception e) {  
+//			    	//NoSuchMethod
+//			    	e.printStackTrace();  
+//			    	return 0;//Doesn´t compare/sort
+//			    } 
+		}
+		
+		public int compare(String o1, String o2) {
+			if(null == o1) {
+				if(null == o2) {
+					return 0;
+				} else {
+					return -1;
+				}
+			} else {
+				if(null == o2) {
+					return 1;
+				} else {
+					return o1.toUpperCase().compareTo(o2.toUpperCase());
+				}
+			}
+		}
+		
+}
+
--- clinical-scenarios/src/main/java/org/mdpnp/clinicalscenarios/server/scenario/ScenarioEntity.java
+++ clinical-scenarios/src/main/java/org/mdpnp/clinicalscenarios/server/scenario/ScenarioEntity.java
@@ -5,6 +5,8 @@ import static org.mdpnp.clinicalscenarios.server.OfyService.ofy;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.mdpnp.clinicalscenarios.client.scenario.ScenarioPanel;
+
 import com.google.appengine.api.datastore.QueryResultIterator;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
@@ -21,15 +23,15 @@ import com.googlecode.objectify.annotation.OnSave;
 @Entity
 public class ScenarioEntity implements java.io.Serializable {
 	
-	private final static String SCN_STATUS_UNSUBMITTED = 	"unsubmitted";//created and/or modified, but not yet submitted for approval
-	private final static String SCN_STATUS_SUBMITTED = 		"submitted"; //submitted for approval, not yet revised nor approved 
-	private final static String SCN_STATUS_APPROVED = 		"approved"; //revised and approved
-	private final static String SCN_STATUS_REJECTED = 		"rejected"; //revised but not approved. Rejected for revision 
+//	private final static String SCN_STATUS_UNSUBMITTED = 	"unsubmitted";//created and/or modified, but not yet submitted for approval
+//	private final static String SCN_STATUS_SUBMITTED = 		"submitted"; //submitted for approval, not yet revised nor approved 
+//	private final static String SCN_STATUS_APPROVED = 		"approved"; //revised and approved
+//	private final static String SCN_STATUS_REJECTED = 		"rejected"; //revised but not approved. Rejected for revision 
 	
 	@Id
 	private Long id;
 	
-	//XXX @ Index Shouldn't we index the title if we are going to base our basic search on it?
+	//XXX @ Index  --Shouldn't we index the title if we are going to base our basic search on it?
 	private String title; //title of the scenario
 	@Index
 	private String status; //status of the scenario (modified, submitted, ect...)
@@ -127,7 +129,7 @@ public class ScenarioEntity implements java.io.Serializable {
 		
 	public static ScenarioEntity create() {
 	    ScenarioEntity s = new ScenarioEntity();
-	    s.setStatus(SCN_STATUS_UNSUBMITTED);//By default, pending of submission
+	    s.setStatus(ScenarioPanel.SCN_STATUS_UNSUBMITTED);//By default, pending of submission
 		//to ID the current user
 	    UserService userService = UserServiceFactory.getUserService();
 	    User user = userService.getCurrentUser();//final? What happens when we change the user?
@@ -149,19 +151,58 @@ public class ScenarioEntity implements java.io.Serializable {
 	    return ofy().load().type(ScenarioEntity.class).id(id).now();
 	}
 	
+	
+	/* XXX diego@mdpnp.org
+	 * One concern about the methods herein published for searches: Am I bringing too much business logic to this point?
+	 * Should the be simpler searches that I would filter later?
+	 */
+	
+	/**
+	 * Searches scenarios using the keywords
+	 * @param keywords
+	 * @return
+	 */
 	public static List<ScenarioEntity> searchByKeywords(String keywords) {
-		List<ScenarioEntity> scenarios = findAllScenarios();
+//		List<ScenarioEntity> scenarios = findAllScenarios();//XXX diego@mdpnp.org The basic search should be among APPROVED Scn, and not all of them
+		List<ScenarioEntity> scenarios = searchByStatus(ScenarioPanel.SCN_STATUS_APPROVED);
 		List<ScenarioEntity> matchingScenarios = new ArrayList<ScenarioEntity>();
 		
 		String str = keywords.toUpperCase();
-		
+		try{
+
 		for(ScenarioEntity s : scenarios) {
+			//1- Check title
 			String title = s.getTitle();
 			title = null == title ? "" : title;
 			if(title.toUpperCase().contains(str)) {
 				matchingScenarios.add(s);
-//				logger.info("Search RefId:"+System.identityHashCode(s)+" Id:"+s.getId()+" Title:"+s.getTitle());
+			}else{
+				//2- Check background
+				String currentState = s.getBackground().getCurrentState()==null ? "" : s.getBackground().getCurrentState();
+				String proposedState = s.getBackground().getProposedState()==null ? "" : s.getBackground().getProposedState();
+				if(currentState.toUpperCase().contains(str) || proposedState.toUpperCase().contains(str)){
+					matchingScenarios.add(s);
+				}else{
+					//3- Check Benefits and risks
+					String benefits = s.getBenefitsAndRisks().getBenefits()==null ? "" : s.getBenefitsAndRisks().getBenefits();
+					String risks = s.getBenefitsAndRisks().getRisks()==null ? "" : s.getBenefitsAndRisks().getRisks();
+					if(benefits.toUpperCase().contains(str) || risks.toUpperCase().contains(str)){
+						matchingScenarios.add(s);
+					}else{
+						//4- check Proposed Solution
+						String algorithm = s.getProposedSolution().getAlgorithm()==null ? "" : s.getProposedSolution().getAlgorithm();
+						String process = s.getProposedSolution().getProcess()==null ? "" : s.getProposedSolution().getProcess();
+						if(algorithm.toUpperCase().contains(str) || process.toUpperCase().contains(str))
+							matchingScenarios.add(s);
+					}
+				}
+				
 			}
+
+		}
+		
+		}catch(Exception e){
+			e.printStackTrace();
 		}
 		return matchingScenarios;
 	}
