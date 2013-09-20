23a34592a9f8c7a1d0dcc788e8c898fdeacf2e14
jeffplourde <jeff@mdpnp.org>
2013-09-20 15:00:55 -0400

    apparently openjdk sampled sound is not as good at inferring waveform formats so made it explicit

--- interop-lab/demo-apps/src/main/java/org/mdpnp/apps/testapp/pca/PCAPanel.java
+++ interop-lab/demo-apps/src/main/java/org/mdpnp/apps/testapp/pca/PCAPanel.java
@@ -9,9 +9,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.concurrent.ScheduledExecutorService;
 
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JPanel;
@@ -29,13 +31,13 @@ public class PCAPanel extends JPanel implements VitalModelListener {
 
     private final PCAConfig pcaConfig;
 //    private final VitalMonitoring vitalMonitor;
-    
+
     private static final Border EMPTY_BORDER = new EmptyBorder(15,15,15,15);
     private static final Border YELLOW_BORDER = new LineBorder(Color.yellow, 15, false);
     private static final Border RED_BORDER = new LineBorder(Color.red, 15, false);
-    
+
     private Clip drugDeliveryAlarm, generalAlarm;
-    
+
     private static final InputStream inMemory(InputStream is) throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         byte[] buf = new byte[2048];
@@ -47,41 +49,54 @@ public class PCAPanel extends JPanel implements VitalModelListener {
         is.close();
         return new ByteArrayInputStream(baos.toByteArray());
     }
-    
-    
+
+
     public PCAPanel(ScheduledExecutorService refreshScheduler) {
 //        super(JSplitPane.HORIZONTAL_SPLIT, true, new PCAConfig(refreshScheduler), new VitalMonitoring(refreshScheduler));
         super(new BorderLayout());
 //        pcaConfig = (PCAConfig) getLeftComponent();
 //        vitalMonitor = (VitalMonitoring) getRightComponent();
         pcaConfig = new PCAConfig(refreshScheduler);
-        
+
         setBorder(EMPTY_BORDER);
-        
+
 //        pcaConfig.setBackground(Color.orange);
-        
+
         add(pcaConfig, BorderLayout.CENTER);
 
 //        setDividerSize(4);
 
 //        setDividerLocation(0.5);
-        
+
         try {
             // http://www.anaesthesia.med.usyd.edu.au/resources/alarms/
-            
+
             // Per the documentation for AudioSystem.getAudioInputStream mark/reset on the stream is required
             // so we'll load it into memory because I stored the audio clips in a Jar file and the inflating
             // InputStream does not support mark/reset
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(inMemory(PCAPanel.class.getResourceAsStream("drughi.wav")));
-            drugDeliveryAlarm = AudioSystem.getClip();
+            AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
+                    22050.0f,
+                    16, 1, 2,
+                    AudioSystem.NOT_SPECIFIED, false);
+            DataLine.Info info = new DataLine.Info(Clip.class, format);
+            drugDeliveryAlarm = (Clip) AudioSystem.getLine(info);
             drugDeliveryAlarm.open(audioInputStream);
             drugDeliveryAlarm.setLoopPoints(0, -1);
             System.out.println(drugDeliveryAlarm.getFrameLength());
+            System.out.println(drugDeliveryAlarm.getFormat());
+
+            format = new AudioFormat(AudioFormat.Encoding.PCM_UNSIGNED,
+                    22050.0f,
+                    8, 1, 1,
+                    AudioSystem.NOT_SPECIFIED, true);
+            info = new DataLine.Info(Clip.class, format);
             audioInputStream = AudioSystem.getAudioInputStream(inMemory(PCAPanel.class.getResourceAsStream("genhi.wav")));
-            generalAlarm = AudioSystem.getClip();
+            generalAlarm = (Clip) AudioSystem.getLine(info);
             generalAlarm.open(audioInputStream);
             generalAlarm.setLoopPoints(0, -1);
-            
+            System.out.println(generalAlarm.getFormat());
+
         } catch (UnsupportedAudioFileException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -90,10 +105,10 @@ public class PCAPanel extends JPanel implements VitalModelListener {
             e.printStackTrace();
         }
     }
-    
+
 
     private VitalModel model;
-    
+
     public void setModel(VitalModel vitalModel, PumpModel pumpModel) {
         if(this.model != null) {
             this.model.removeListener(this);
@@ -139,9 +154,9 @@ public class PCAPanel extends JPanel implements VitalModelListener {
             default:
             }
         }
-        
-        
-        
+
+
+
         if(model.isInfusionStopped() || model.getState().equals(VitalModel.State.Alarm)) {
             setBorder(RED_BORDER);
         } else if(VitalModel.State.Warning.equals(model.getState())) {
@@ -149,7 +164,7 @@ public class PCAPanel extends JPanel implements VitalModelListener {
         } else {
             setBorder(EMPTY_BORDER);
         }
-        
+
     }
 
 
@@ -163,5 +178,5 @@ public class PCAPanel extends JPanel implements VitalModelListener {
     public void vitalAdded(VitalModel model, Vital vital) {
         vitalChanged(model, vital);
     }
-    
+
 }
