e9330810792ba28b0db7c172511a392568f40200
jeffplourde <jeff@mdpnp.org>
2013-10-03 11:32:19 -0400

    handle association refuse which has no presentation data unit. also was terminating the watchdog on disconnect before it could emit a finish

--- devices/philips/src/main/java/org/mdpnp/devices/philips/intellivue/CompoundProtocol.java
+++ devices/philips/src/main/java/org/mdpnp/devices/philips/intellivue/CompoundProtocol.java
@@ -11,40 +11,50 @@ import org.mdpnp.devices.philips.intellivue.connectindication.ConnectIndicationP
 import org.mdpnp.devices.philips.intellivue.dataexport.DataExportMessage;
 import org.mdpnp.devices.philips.intellivue.dataexport.DataExportProtocol;
 import org.mdpnp.devices.philips.intellivue.dataexport.impl.DataExportProtocolImpl;
+import org.mdpnp.devices.philips.intellivue.util.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CompoundProtocol implements Protocol {
-	private final DataExportProtocol dataExportProtocol = new DataExportProtocolImpl();
-	private final ConnectIndicationProtocol connectIndicationProtocol = new ConnectIndicationProtocolImpl();
-	private final AssociationProtocol associationProtocol = new AssociationProtocolImpl();
-	@Override
-	public Message parse(ByteBuffer bb) {
-		if(bb.hasRemaining()) {
-			int firstByte = 0xFF & bb.get(0);
-			
-			switch(firstByte) {
-			case 0xE1:
-				return dataExportProtocol.parse(bb);
-			case 0x00:
-				return connectIndicationProtocol.parse(bb);
-			default:
-				return associationProtocol.parse(bb);
-			}
-		} else {
-			return null;
-		}
-	}
-	@Override
-	public void format(Message message, ByteBuffer bb) {
-		if(message instanceof DataExportMessage) {
-			dataExportProtocol.format((DataExportMessage)message, bb);
-		} else if(message instanceof AssociationMessage) {
-			associationProtocol.format((AssociationMessage)message, bb);
-		} else if(message instanceof ConnectIndication) {
-			connectIndicationProtocol.format((ConnectIndication) message, bb);
-		} else {
-			// TODO something to alert the user
-		}
-	}
-		
+    private final static Logger log = LoggerFactory.getLogger(CompoundProtocol.class);
+    private final DataExportProtocol dataExportProtocol = new DataExportProtocolImpl();
+    private final ConnectIndicationProtocol connectIndicationProtocol = new ConnectIndicationProtocolImpl();
+    private final AssociationProtocol associationProtocol = new AssociationProtocolImpl();
+    @Override
+    public Message parse(ByteBuffer bb) {
+        try {
+            if(bb.hasRemaining()) {
+                int firstByte = 0xFF & bb.get(0);
+
+                switch(firstByte) {
+                case 0xE1:
+                    return dataExportProtocol.parse(bb);
+                case 0x00:
+                    return connectIndicationProtocol.parse(bb);
+                default:
+                    return associationProtocol.parse(bb);
+                }
+            } else {
+                return null;
+            }
+        } catch (RuntimeException re) {
+            bb.position(0);
+            log.trace("Offending buffer:\n"+Util.dump(bb, 20));
+            throw new RuntimeException(re);
+        }
+    }
+    @Override
+    public void format(Message message, ByteBuffer bb) {
+        if(message instanceof DataExportMessage) {
+            dataExportProtocol.format((DataExportMessage)message, bb);
+        } else if(message instanceof AssociationMessage) {
+            associationProtocol.format((AssociationMessage)message, bb);
+        } else if(message instanceof ConnectIndication) {
+            connectIndicationProtocol.format((ConnectIndication) message, bb);
+        } else {
+            // TODO something to alert the user
+        }
+    }
+
 
 }
