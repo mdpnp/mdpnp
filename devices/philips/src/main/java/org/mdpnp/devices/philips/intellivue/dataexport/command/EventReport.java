eb41f685a8bcd22cac36fad8934790a468537685
jeffplourde <jeff@mdpnp.org>
2013-10-11 12:01:29 -0400

    more correct to use the EventReportResult type .. even though it's identical to EventReport

--- devices/philips/src/main/java/org/mdpnp/devices/philips/intellivue/dataexport/command/EventReport.java
+++ devices/philips/src/main/java/org/mdpnp/devices/philips/intellivue/dataexport/command/EventReport.java
@@ -2,14 +2,16 @@ package org.mdpnp.devices.philips.intellivue.dataexport.command;
 
 import org.mdpnp.devices.philips.intellivue.data.ManagedObjectIdentifier;
 import org.mdpnp.devices.philips.intellivue.data.OIDType;
+import org.mdpnp.devices.philips.intellivue.data.RelativeTime;
 import org.mdpnp.devices.philips.intellivue.dataexport.DataExportCommand;
 import org.mdpnp.devices.philips.intellivue.dataexport.DataExportEvent;
 
 public interface EventReport extends DataExportCommand {
-	ManagedObjectIdentifier getManagedObject();
-	OIDType getEventType();
-	void setEventType(OIDType oid);
-	EventReport createConfirm();
-	DataExportEvent getEvent();
-	void setEvent(DataExportEvent event);
+    ManagedObjectIdentifier getManagedObject();
+    OIDType getEventType();
+    void setEventType(OIDType oid);
+    EventReportResult createConfirm();
+    DataExportEvent getEvent();
+    RelativeTime getEventTime();
+    void setEvent(DataExportEvent event);
 }
