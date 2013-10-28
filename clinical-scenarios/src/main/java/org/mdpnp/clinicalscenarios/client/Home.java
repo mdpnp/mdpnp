939dbd60ce78f3e5a1976ccc65a7a1d294f051b2
dalonso <diego@mdpnp.org>
2013-10-28 10:19:59 -0400

    updated version number/disclaimer

--- clinical-scenarios/src/main/java/org/mdpnp/clinicalscenarios/client/Home.java
+++ clinical-scenarios/src/main/java/org/mdpnp/clinicalscenarios/client/Home.java
@@ -1,7 +1,6 @@
 package org.mdpnp.clinicalscenarios.client;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.Element;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
