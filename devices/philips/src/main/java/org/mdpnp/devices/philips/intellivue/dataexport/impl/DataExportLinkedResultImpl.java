50b0b10a43b27524d01363b7a1aef285123c8670
Jeff Plourde <jeff@mdpnp.org>
2014-02-21 15:36:38 -0500

    the case where a continuation is received of a message that was aborted (or otherwise lost)

--- devices/philips/src/main/java/org/mdpnp/devices/philips/intellivue/dataexport/impl/DataExportLinkedResultImpl.java
+++ devices/philips/src/main/java/org/mdpnp/devices/philips/intellivue/dataexport/impl/DataExportLinkedResultImpl.java
@@ -11,56 +11,83 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DataExportLinkedResultImpl extends DataExportResultImpl implements DataExportLinkedResult {
-	private RemoteOperationLinkedState state;
-	private short count;
-	
-	@Override
-	public RemoteOperationLinkedState getLinkedState() {
-		return state;
-	}
-	@Override
-	public short getLinkedCount() {
-		return count;
-	}
-	
-	public static final int peekInvokeId(ByteBuffer bb) {
-		return 0xFFFF & bb.getShort(bb.position() + 2);
-	}
-	private static final Logger log = LoggerFactory.getLogger(DataExportLinkedResultImpl.class);
-	@Override
-	public void parse(ByteBuffer bb) {
-		state = RemoteOperationLinkedState.valueOf(Bits.getUnsignedByte(bb));
-		count = Bits.getUnsignedByte(bb);
-		invokeId = Bits.getUnsignedShort(bb);
-		int cmdType;
-		commandType = CommandType.valueOf(cmdType = Bits.getUnsignedShort(bb));
-		int length = Bits.getUnsignedShort(bb);
-		if(commandType == null) {
-			// TODO error ish
-			log.warn("Unrecognized command type " + cmdType);
-			bb.position(bb.position() + length);
-		} else {
-			switch(state) {
-			case First:
-				command = CommandFactory.buildCommand(commandType, true);
-				command.setMessage(this);
-				command.parse(bb);
-				break;
-			case NotFirstNotLast:
-				command.parseMore(bb);
-				break;
-			case Last:
-				command.parseMore(bb);
-				break;
-			}
-		}
-		
-	}
-	
-	@Override
-	public void format(ByteBuffer bb) {
-		Bits.putUnsignedByte(bb, state.asShort());
-		Bits.putUnsignedByte(bb, count);
-		super.format(bb);
-	}
+    private RemoteOperationLinkedState state;
+    private short count;
+
+    @Override
+    public RemoteOperationLinkedState getLinkedState() {
+        return state;
+    }
+    @Override
+    public short getLinkedCount() {
+        return count;
+    }
+
+    public static final int peekInvokeId(ByteBuffer bb) {
+        return 0xFFFF & bb.getShort(bb.position() + 2);
+    }
+
+    private static final Logger log = LoggerFactory.getLogger(DataExportLinkedResultImpl.class);
+
+    private static final void advanceOrEnd(ByteBuffer bb, int advancement) {
+        int newPosition = bb.position() + advancement;
+        if(newPosition>bb.limit()) {
+            log.warn("Tried to advance " + advancement + " bytes where only " + bb.remaining() + " remain");
+            bb.position(bb.limit());
+        } else {
+            bb.position(newPosition);
+        }
+    }
+    
+    @Override
+    public void parse(ByteBuffer bb) {
+        state = RemoteOperationLinkedState.valueOf(Bits.getUnsignedByte(bb));
+        count = Bits.getUnsignedByte(bb);
+        invokeId = Bits.getUnsignedShort(bb);
+        int cmdType;
+        commandType = CommandType.valueOf(cmdType = Bits.getUnsignedShort(bb));
+        int length = Bits.getUnsignedShort(bb);
+        if (commandType == null) {
+            // TODO error ish
+            log.warn("Unrecognized command type " + cmdType);
+            advanceOrEnd(bb, length);
+        } else {
+            switch (state) {
+            case First:
+                command = CommandFactory.buildCommand(commandType, true);
+                if(null == command) {
+                    log.warn("Unable to build command for CommandType="+commandType);
+                    advanceOrEnd(bb, length);
+                } else {
+                    command.setMessage(this);
+                    command.parse(bb);
+                }
+                break;
+            case NotFirstNotLast:
+                if(null == command) {
+                    log.warn("Received a command of type " + commandType + " with NotFirstNotLastState but no previous First");
+                    advanceOrEnd(bb, length);
+                } else {
+                    command.parseMore(bb);
+                }
+                break;
+            case Last:
+                if(null == command) {
+                    log.warn("Received a command of type " + commandType + " with Last but no previous First");
+                    advanceOrEnd(bb, length);
+                } else {
+                    command.parseMore(bb);
+                }
+                break;
+            }
+        }
+
+    }
+
+    @Override
+    public void format(ByteBuffer bb) {
+        Bits.putUnsignedByte(bb, state.asShort());
+        Bits.putUnsignedByte(bb, count);
+        super.format(bb);
+    }
 }
