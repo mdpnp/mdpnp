a2a2a4d24d3741b9bc6914ee49473fce32f092dd
Jeff Plourde <jeff@mdpnp.org>
2014-03-11 18:28:58 -0400

    No longer using Java Media Framework of JFreeChart

--- interop-lab/demo-apps/src/main/java/org/mdpnp/apps/testapp/RoomSyncPanel.java
+++ interop-lab/demo-apps/src/main/java/org/mdpnp/apps/testapp/RoomSyncPanel.java
@@ -6,7 +6,6 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
-import com.jeffplourde.util.gui.table.XTable;
 import com.rti.dds.subscription.Subscriber;
 
 @SuppressWarnings("serial")
@@ -22,7 +21,7 @@ public class RoomSyncPanel extends JPanel {
 //		} catch (Exception e) {
 //			throw new RuntimeException(e);
 //		}
-        XTable table = new XTable(null, Integer.MAX_VALUE, false, false, false, true);
+        JTable table = new JTable();
         add(new JScrollPane(table), BorderLayout.CENTER);
         table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
     }
