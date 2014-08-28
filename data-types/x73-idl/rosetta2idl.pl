d54277e884e1f63f71dbd070fd47cf079df1eaba
Jeff Plourde <jeff@mdpnp.org>
2014-08-28 15:17:53 -0400

    simplified this script to use all harmonized rosetta refids

--- data-types/x73-idl/rosetta2idl.pl
+++ data-types/x73-idl/rosetta2idl.pl
@@ -3,33 +3,24 @@
 use Text::ParseWords;
 
 # generates IDL from NIST Rosetta information found at:
-# http://hit-testing.nist.gov:13110/rtmms/getCSVRosettaVendor.do?currentView=false&vendor=All&name=rosetta_terms&filter=%5B%5D&start=0&limit=50
+# https://rtmms.nist.gov/rtmms/index.htm
 
 $_ = <>;
 my @headers = split ",";
 my %addresses;
 
 my $REFID;
-my $CODE10;
-my $Standard_Table;
-
-#print join(",", @headers);
 
 for(my $i = 0; $i <= $#headers; $i++) {
-#    print "$i \t $headers[$i]\n";
     if("REFID" eq $headers[$i]) {
   $REFID = $i;
-    } elsif("CODE10" eq $headers[$i]) {
-  $CODE10 = $i;
     } elsif("Standard_Table" eq $headers[$i]) {
   $Standard_Table = $i;
     }
+  
 }
 
 my %nomenclature;
-my %codes;
-
-#print "REFID=$REFID CODE10=$CODE10 Standard_Table=$Standard_Table\n";
 
 print "#ifndef _ROSETTA_IDL_\n";
 print "#define _ROSETTA_IDL_\n";
@@ -37,44 +28,23 @@ print "\n";
 print "module rosetta {\n";
 
 my $mlen = 0;
-
+my $count = 0;
 while(<>) {
     my @data = Text::ParseWords::parse_line(',', 0, $_);
     my $stdtable = $data[$Standard_Table];
     my $r = $data[$REFID];
-    my $c = $data[$CODE10];
 
-    if($mlen < length($r)) {
-	$mlen = length($r);
-	}
-
-#    print "$stdtable $r $c\n";
-
-#    ($stdtable =~ /^$/) && next;
     ($r =~ /^$/) && next;
-    ($c =~ /^$/) && next;
-
+    $r =~ s/[^A-Za-z0-9_]//g;
     if(exists $nomenclature{$r}) {
-  if(!$nomenclature{$r} eq $c) {
-      print STDERR "Redefinition of $r; was $nomenclature{$r}, now $c\n";
-  } else {
-#	    print STDERR "Redefinition of $r; harmless\n";
-  }
-  next;
-    }
-    if(exists $codes{$c}) {
-  if(!$codes{$c} eq $r) {
-      print STDERR "Redefinition of $c; was $codes{$c}, now $r\n";
-  }
-  next;
+      print STDERR "Redefinition of $r\n";
+      next;
     }
 
-    $codes{$c} = $r;
-    $nomenclature{$r} = $c;
+    $nomenclature{$r} = $r;
     print "  // from $stdtable\n";
-#    print "  const unsigned long $r = $c;\n";
-#    print "  $r = $c,\n";
      print "  const string $r = \"$r\";\n";
+    $count++;
 
 }
 
@@ -82,4 +52,4 @@ print "};\n";
 print "#endif\n";
 print "\n";
 
-print STDERR "$mlen\n";
+print STDERR "$count codes\n";
