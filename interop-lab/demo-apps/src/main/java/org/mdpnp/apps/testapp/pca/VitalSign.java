31eb250d5e7f3ce341bb3ae6fdef4e0665fdb603
Jeff Plourde <jeff@mdpnp.org>
2014-09-11 23:52:18 -0400

    I'm told pulse oximeters should alarm when SpO2 exceeds 100 so I made that the default.  These settings are entirely configurable though nobody seems interested in setting them so I'll try to make reasonable defaults.

--- interop-lab/demo-apps/src/main/java/org/mdpnp/apps/testapp/pca/VitalSign.java
+++ interop-lab/demo-apps/src/main/java/org/mdpnp/apps/testapp/pca/VitalSign.java
@@ -24,7 +24,7 @@ import org.mdpnp.apps.testapp.vital.VitalModel;
 public enum VitalSign {
     HeartRate("Heart Rate", "bpm", new String[] { rosetta.MDC_PULS_OXIM_PULS_RATE.VALUE, rosetta.MDC_PULS_RATE_NON_INV.VALUE,
             rosetta.MDC_BLD_PULS_RATE_INV.VALUE, rosetta.MDC_ECG_CARD_BEAT_RATE.VALUE, rosetta.MDC_ECG_HEART_RATE.VALUE }, 40f, 140f, 20f, 160f, 0f, 250f, 5000L, 5000L, Color.green), SpO2(
-            "SpO\u2082", "%", new String[] { rosetta.MDC_PULS_OXIM_SAT_O2.VALUE }, 95f, null, 85f, null, 50f, 100f, 5000L, 5000L, Color.pink), RespiratoryRate(
+            "SpO\u2082", "%", new String[] { rosetta.MDC_PULS_OXIM_SAT_O2.VALUE }, 95f, 100f, 85f, 100f, 50f, 100f, 5000L, 5000L, Color.pink), RespiratoryRate(
             "Resp Rate", "bpm", new String[] { rosetta.MDC_RESP_RATE.VALUE, rosetta.MDC_TTHOR_RESP_RATE.VALUE, rosetta.MDC_CO2_RESP_RATE.VALUE }, 10f, 18f, 4f, 35f, 0f, 40f, 5000L, 5000L, Color.yellow), EndTidalCO2(
             "etCO\u2082", "mmHg", new String[] { rosetta.MDC_AWAY_CO2_ET.VALUE }, 20f, 45f, 10f, 75f, 0f, 120f, 5000L, 5000L, Color.yellow), Temperature(
             "Temp", "\u00B0C", new String[] { rosetta.MDC_TEMP_BLD.VALUE }, 35f, 39f, 32f, 42f, 20f, 60f, 5000L, 5000L, Color.black);
