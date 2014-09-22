dd2530c9eca61c8d4352caa11fc332718b4b50d9
Jeff Plourde <jeff@mdpnp.org>
2014-09-22 13:36:13 -0400

    ununsed import

--- interop-lab/demo-apps/src/main/java/org/mdpnp/apps/testapp/MainMenuPanel.java
+++ interop-lab/demo-apps/src/main/java/org/mdpnp/apps/testapp/MainMenuPanel.java
@@ -18,7 +18,6 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
