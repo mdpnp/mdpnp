3a550903c02c9c4613375efcbaeab0771c037985
Jeff Plourde <jeff@mdpnp.org>
2014-10-06 16:31:49 -0400

    promote to 0.4.1

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -1,4 +1,4 @@
-PARCEL=demo-apps-0.4.0
+PARCEL=demo-apps-0.4.1
 TARGETS=`cat targets | sed '/^\s*#/d'`
 # Add --offline to the following line if no internet access
 ./gradlew :interop-lab:demo-apps:distZip
