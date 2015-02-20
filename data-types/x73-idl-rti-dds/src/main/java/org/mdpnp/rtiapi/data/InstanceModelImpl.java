983565cdac1bd8f6fb8cfbf54fc749b7aa284c84
Jeff Plourde <jeff@mdpnp.org>
2015-02-20 15:24:41 -0500

    null QoS info will use defaults; also added convenience overload that doesn't require QoS library and profile info.

--- data-types/x73-idl-rti-dds/src/main/java/org/mdpnp/rtiapi/data/InstanceModelImpl.java
+++ data-types/x73-idl-rti-dds/src/main/java/org/mdpnp/rtiapi/data/InstanceModelImpl.java
@@ -259,6 +259,10 @@ public class InstanceModelImpl<D extends Copyable, R extends DataReaderImpl> ext
         }
     }
 
+    @Override
+    public void start(Subscriber subscriber, EventLoop eventLoop) {
+        start(subscriber, eventLoop, null, null, null, null);
+    }
 
     @Override
     public void start(Subscriber subscriber, EventLoop eventLoop, String qosLibrary, String qosProfile) {
@@ -285,9 +289,13 @@ public class InstanceModelImpl<D extends Copyable, R extends DataReaderImpl> ext
         subscriber.get_qos(sQos);
         sQos.entity_factory.autoenable_created_entities = false;
         subscriber.set_qos(sQos);
-        
-        reader = (R) subscriber.create_datareader_with_profile(null==filteredTopic?saTopic:filteredTopic, qosLibrary,
-                qosProfile, null, StatusKind.STATUS_MASK_NONE);
+        if(null == qosProfile || null == qosLibrary) {
+            reader = (R) subscriber.create_datareader(null==filteredTopic?saTopic:filteredTopic, Subscriber.DATAREADER_QOS_DEFAULT, 
+                    null, StatusKind.STATUS_MASK_NONE);
+        } else {
+            reader = (R) subscriber.create_datareader_with_profile(null==filteredTopic?saTopic:filteredTopic, qosLibrary,
+                    qosProfile, null, StatusKind.STATUS_MASK_NONE);
+        }
         sQos.entity_factory.autoenable_created_entities = true;
         subscriber.set_qos(sQos);
 
