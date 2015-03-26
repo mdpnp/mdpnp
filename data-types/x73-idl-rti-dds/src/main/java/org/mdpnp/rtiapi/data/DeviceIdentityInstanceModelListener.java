62ae303db9118c96ef02314b947154c10dc1bc1f
Jeff Plourde <jeff@mdpnp.org>
2015-03-25 21:08:39 -0400

    Adds a defined listener for DeviceIdentityInstanceModel (for convenience)

---
+++ data-types/x73-idl-rti-dds/src/main/java/org/mdpnp/rtiapi/data/DeviceIdentityInstanceModelListener.java
@@ -0,0 +1,8 @@
+package org.mdpnp.rtiapi.data;
+
+import ice.DeviceIdentity;
+import ice.DeviceIdentityDataReader;
+
+public interface DeviceIdentityInstanceModelListener extends InstanceModelListener<DeviceIdentity, DeviceIdentityDataReader> {
+
+}
