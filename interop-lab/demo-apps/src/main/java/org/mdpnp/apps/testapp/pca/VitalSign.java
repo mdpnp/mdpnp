3416ff09c51f0b453c15a055110f48a0644462dc
Jeff Plourde <jeff@mdpnp.org>
2015-04-11 11:35:56 -0400

    Add pressures as vital sign options

--- interop-lab/demo-apps/src/main/java/org/mdpnp/apps/testapp/pca/VitalSign.java
+++ interop-lab/demo-apps/src/main/java/org/mdpnp/apps/testapp/pca/VitalSign.java
@@ -22,24 +22,77 @@ import org.mdpnp.apps.testapp.vital.VitalModel;
  *
  */
 public enum VitalSign {
-    HeartRate("Heart Rate", "bpm", new String[] { 
-            rosetta.MDC_PULS_OXIM_PULS_RATE.VALUE, rosetta.MDC_PULS_RATE_NON_INV.VALUE,
-            rosetta.MDC_BLD_PULS_RATE_INV.VALUE, rosetta.MDC_ECG_CARD_BEAT_RATE.VALUE, 
-            rosetta.MDC_ECG_HEART_RATE.VALUE, rosetta.MDC_PULS_RATE.VALUE,
-            rosetta.MDC_BLD_PULS_RATE_INV.VALUE}, 
-            40.0, 140.0, 20.0, 160.0, 0.0, 250.0, 5000L, 5000L, Color.green), 
-            
-    SpO2(
-            "SpO\u2082", "%", new String[] { rosetta.MDC_PULS_OXIM_SAT_O2.VALUE }, 95.0, 100.0, 85.0, 100.0, 50.0, 100.0, 5000L, 5000L, Color.pink), RespiratoryRate(
-            "Resp Rate", "bpm", new String[] { 
-                    rosetta.MDC_RESP_RATE.VALUE, 
-                    rosetta.MDC_TTHOR_RESP_RATE.VALUE, 
-                    rosetta.MDC_CO2_RESP_RATE.VALUE, 
-                    rosetta.MDC_VENT_RESP_RATE.VALUE, 
-                    rosetta.MDC_AWAY_RESP_RATE.VALUE }, 
-                    10.0, 25.0, 4.0, 35.0, 0.0, 40.0, 5000L, 5000L, Color.yellow), EndTidalCO2(
-            "etCO\u2082", "mmHg", new String[] { rosetta.MDC_AWAY_CO2_ET.VALUE }, 20.0, 45.0, 10.0, 75.0, 0.0, 120.0, 5000L, 5000L, Color.yellow), Temperature(
-            "Temp", "\u00B0C", new String[] { rosetta.MDC_TEMP_BLD.VALUE }, 35.0, 39.0, 32.0, 42.0, 20.0, 60.0, 5000L, 5000L, Color.black);
+    HeartRate("Heart Rate", "bpm", new String[] { rosetta.MDC_PULS_OXIM_PULS_RATE.VALUE, rosetta.MDC_PULS_RATE_NON_INV.VALUE,
+            rosetta.MDC_BLD_PULS_RATE_INV.VALUE, rosetta.MDC_ECG_CARD_BEAT_RATE.VALUE, rosetta.MDC_ECG_HEART_RATE.VALUE, rosetta.MDC_PULS_RATE.VALUE,
+            rosetta.MDC_BLD_PULS_RATE_INV.VALUE }, 40.0, 140.0, 20.0, 160.0, 0.0, 250.0, 5000L, 5000L, Color.green),
+    SpO2("SpO\u2082", "%", new String[] { rosetta.MDC_PULS_OXIM_SAT_O2.VALUE }, 95.0, 100.0, 85.0, 100.0, 50.0, 100.0, 5000L, 5000L, Color.pink), 
+    RespiratoryRate("Resp Rate", "bpm", new String[] { rosetta.MDC_RESP_RATE.VALUE, rosetta.MDC_TTHOR_RESP_RATE.VALUE, rosetta.MDC_CO2_RESP_RATE.VALUE,
+                    rosetta.MDC_VENT_RESP_RATE.VALUE, rosetta.MDC_AWAY_RESP_RATE.VALUE }, 10.0, 25.0, 4.0, 35.0, 0.0, 40.0, 5000L, 5000L,
+                    Color.yellow), 
+    EndTidalCO2("etCO\u2082", "mmHg", new String[] { rosetta.MDC_AWAY_CO2_ET.VALUE }, 20.0, 45.0, 10.0, 75.0, 0.0, 120.0,
+                5000L, 5000L, Color.yellow), 
+    Temperature("Temp", "\u00B0C", new String[] { rosetta.MDC_TEMP_BLD.VALUE }, 35.0, 39.0, 32.0, 42.0, 20.0,
+                60.0, 5000L, 5000L, Color.black),
+    InvSystolic("Invasive Systolic", "mmHg", new String[] { 
+            rosetta.MDC_PRESS_BLD_SYS.VALUE,
+            rosetta.MDC_PRESS_BLD_AORT_SYS.VALUE,
+            rosetta.MDC_PRESS_BLD_ART_SYS.VALUE,
+            rosetta.MDC_PRESS_BLD_ART_ABP_SYS.VALUE,
+            rosetta.MDC_PRESS_INTRA_CRAN_SYS.VALUE,
+            rosetta.MDC_PRESS_BLD_ART_FEMORAL_SYS.VALUE,
+            rosetta.MDC_PRESS_BLD_ART_PULM_SYS.VALUE,
+            rosetta.MDC_PRESS_BLD_ART_UMB_SYS.VALUE,
+            rosetta.MDC_PRESS_BLD_ATR_LEFT_SYS.VALUE,
+            rosetta.MDC_PRESS_BLD_ATR_RIGHT_SYS.VALUE,
+            rosetta.MDC_PRESS_BLD_PULM_CAP_SYS.VALUE,
+            rosetta.MDC_PRESS_BLD_VEN_UMB_SYS.VALUE,
+            rosetta.MDC_PRESS_BLD_VENT_LEFT_SYS.VALUE,
+            rosetta.MDC_PRESS_BLD_VENT_RIGHT_SYS.VALUE,
+    }, 90.0, 140.0, 60.0, 180.0, 0, 250.0, 5000L, 5000L, Color.red),
+    InvDiastolic("Invasive Diastolic", "mmHg", new String[] {
+            rosetta.MDC_PRESS_BLD_DIA.VALUE,
+            rosetta.MDC_PRESS_BLD_AORT_DIA.VALUE,
+            rosetta.MDC_PRESS_BLD_ART_DIA.VALUE,
+            rosetta.MDC_PRESS_BLD_ART_ABP_DIA.VALUE,
+            rosetta.MDC_PRESS_INTRA_CRAN_DIA.VALUE,
+            rosetta.MDC_PRESS_BLD_ART_FEMORAL_DIA.VALUE,
+            rosetta.MDC_PRESS_BLD_ART_PULM_DIA.VALUE,
+            rosetta.MDC_PRESS_BLD_ART_UMB_DIA.VALUE,
+            rosetta.MDC_PRESS_BLD_ATR_LEFT_DIA.VALUE,
+            rosetta.MDC_PRESS_BLD_ATR_RIGHT_DIA.VALUE,
+            rosetta.MDC_PRESS_BLD_PULM_CAP_DIA.VALUE,
+            rosetta.MDC_PRESS_BLD_VEN_UMB_DIA.VALUE,
+            rosetta.MDC_PRESS_BLD_VENT_LEFT_DIA.VALUE,
+            rosetta.MDC_PRESS_BLD_VENT_RIGHT_DIA.VALUE,
+    }, 60.0, 100.0, 40.0, 120.0, 0, 200.0, 5000L, 5000L, Color.red),
+    InvMean("Invasive Mean", "mmHg", new String[] {
+            rosetta.MDC_PRESS_BLD_MEAN.VALUE,
+            rosetta.MDC_PRESS_BLD_AORT_MEAN.VALUE,
+            rosetta.MDC_PRESS_BLD_ART_MEAN.VALUE,
+            rosetta.MDC_PRESS_BLD_ART_ABP_MEAN.VALUE,
+            rosetta.MDC_PRESS_INTRA_CRAN_MEAN.VALUE,
+            rosetta.MDC_PRESS_BLD_ART_FEMORAL_MEAN.VALUE,
+            rosetta.MDC_PRESS_BLD_ART_PULM_MEAN.VALUE,
+            rosetta.MDC_PRESS_BLD_ART_UMB_MEAN.VALUE,
+            rosetta.MDC_PRESS_BLD_ATR_LEFT_MEAN.VALUE,
+            rosetta.MDC_PRESS_BLD_ATR_RIGHT_MEAN.VALUE,
+            rosetta.MDC_PRESS_BLD_PULM_CAP_MEAN.VALUE,
+            rosetta.MDC_PRESS_BLD_VEN_UMB_MEAN.VALUE,
+            rosetta.MDC_PRESS_BLD_VENT_LEFT_MEAN.VALUE,
+            rosetta.MDC_PRESS_BLD_VENT_RIGHT_MEAN.VALUE,
+    }, 60.0, 100.0, 40.0, 120.0, 0.0, 200.0, 5000L, 5000L, Color.red),
+    NIBPSystolic("NIBP Systolic", "mmHg", new String[] {
+            rosetta.MDC_PRESS_CUFF_SYS.VALUE,
+            rosetta.MDC_PRESS_BLD_NONINV_SYS.VALUE
+    }, 90.0, 140.0, 60.0, 180.0, 0, 250.0, 5000L, 5000L, Color.red),
+    NIBPDiastolic("NIBP Diastolic", "mmHg", new String[] {
+            rosetta.MDC_PRESS_CUFF_DIA.VALUE,
+            rosetta.MDC_PRESS_BLD_NONINV_DIA.VALUE            
+    }, 60.0, 100.0, 40.0, 120.0, 0, 200.0, 5000L, 5000L, Color.red),
+    NIBPMean("NIBP Mean", "mmHg", new String[] {
+            rosetta.MDC_PRESS_CUFF_MEAN.VALUE,
+            rosetta.MDC_PRESS_BLD_NONINV_MEAN.VALUE     
+    }, 60.0, 100.0, 40.0, 120.0, 0.0, 200.0, 5000L, 5000L, Color.red)
     ;
 
     VitalSign(String label, String units, String[] metric_ids, Double startingLow, Double startingHigh, Double criticalLow, Double criticalHigh,
