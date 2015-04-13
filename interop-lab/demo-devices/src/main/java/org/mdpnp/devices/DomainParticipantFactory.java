f5be90026526c37a20a0dff748ccf602595d6528
Jeff Plourde <jeff@mdpnp.org>
2015-04-13 06:54:36 -0500

    Increment and assign a partition id for every participant created by the spring DomainParticipantFactory.  This to avoid delays searching for unused UDP ports.

--- interop-lab/demo-devices/src/main/java/org/mdpnp/devices/DomainParticipantFactory.java
+++ interop-lab/demo-devices/src/main/java/org/mdpnp/devices/DomainParticipantFactory.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.FactoryBean;
 
 import com.rti.dds.domain.DomainParticipant;
 import com.rti.dds.domain.DomainParticipantFactoryQos;
+import com.rti.dds.domain.DomainParticipantQos;
 import com.rti.dds.domain.builtin.ParticipantBuiltinTopicDataTypeSupport;
 import com.rti.dds.infrastructure.StatusKind;
 import com.rti.dds.publication.builtin.PublicationBuiltinTopicDataTypeSupport;
@@ -19,6 +20,8 @@ public class DomainParticipantFactory implements FactoryBean<DomainParticipant>,
         this.domain = domain;
     }
     
+    private static int nextParticipantId = 0;
+    
     @Override
     public DomainParticipant getObject() throws Exception {
         if(null == instance) {
@@ -26,9 +29,15 @@ public class DomainParticipantFactory implements FactoryBean<DomainParticipant>,
             com.rti.dds.domain.DomainParticipantFactory.get_instance().get_qos(qos);
             boolean autoenable = qos.entity_factory.autoenable_created_entities;
             qos.entity_factory.autoenable_created_entities = false;
+            
             com.rti.dds.domain.DomainParticipantFactory.get_instance().set_qos(qos);
             
-            instance = com.rti.dds.domain.DomainParticipantFactory.get_instance().create_participant(domain, com.rti.dds.domain.DomainParticipantFactory.PARTICIPANT_QOS_DEFAULT, null,
+            DomainParticipantQos dpQos = new DomainParticipantQos();
+            
+            com.rti.dds.domain.DomainParticipantFactory.get_instance().get_default_participant_qos(dpQos);
+            dpQos.wire_protocol.participant_id = nextParticipantId++;
+            
+            instance = com.rti.dds.domain.DomainParticipantFactory.get_instance().create_participant(domain, dpQos, null,
                     StatusKind.STATUS_MASK_NONE);
             
             // Initialize this builtin reader
