8a3bd1bc807e75caf41cbf20aa6e1b082dce0754
Jeff Plourde <jeff@mdpnp.org>
2015-04-23 23:23:27 -0400

    Changed back to using Debian 7.8 "stock" beaglebone OS images.

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -6,8 +6,8 @@ TARGETS=`cat targets | sed '/^\s*#/d'`
 for TARGET in $TARGETS
 do
   echo Shipping bundle to $TARGET
-  scp interop-lab/demo-apps/build/distributions/$PARCEL.zip ubuntu@$TARGET: > $TARGET.scp.out &
-  ssh ubuntu@$TARGET rm -fR $PARCEL/
+  scp interop-lab/demo-apps/build/distributions/$PARCEL.zip debian@$TARGET: > $TARGET.scp.out &
+  ssh debian@$TARGET rm -fR $PARCEL/
 done
 echo Awaiting shipment completion
 wait
@@ -15,7 +15,7 @@ wait
 for TARGET in $TARGETS
 do
   echo Decompressing to $TARGET
-  ssh ubuntu@$TARGET unzip $PARCEL.zip > $TARGET.unzip.out &
+  ssh debian@$TARGET unzip $PARCEL.zip > $TARGET.unzip.out &
 done
 echo Awaiting decompression
 wait
@@ -23,13 +23,13 @@ wait
 for TARGET in $TARGETS
 do
   echo Stopping device-adapter on $TARGET
-  ssh ubuntu@$TARGET sudo stop device-adapter
+  ssh debian@$TARGET sudo service device-adapter stop
   echo Copying more files to $TARGET
-  scp log4j.properties ubuntu@$TARGET:
-  scp device-adapter.conf ubuntu@$TARGET:/etc/init/device-adapter.conf
-  ssh ubuntu@$TARGET chmod 777 $PARCEL/bin/OpenICE
+  scp log4j.properties debian@$TARGET:
+  scp device-adapter debian@$TARGET:/etc/init.d/device-adapter
+  ssh debian@$TARGET chmod 777 $PARCEL/bin/OpenICE
   rm $TARGET.scp.out
   rm $TARGET.unzip.out
   echo Restarting device-adapter on $TARGET
-  ssh ubuntu@$TARGET sudo start device-adapter
+  ssh debian@$TARGET sudo service device-adapter start
 done
