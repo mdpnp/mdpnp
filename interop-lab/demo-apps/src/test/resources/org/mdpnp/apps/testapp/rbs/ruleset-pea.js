cbb8245993b5d212e2b48417d3771de7bf0feb6b
j-peterson <j.t.peterson01@gmail.com>
2015-10-21 18:18:02 -0400

    more updates to thresholds and description of pea ruleset

--- interop-lab/demo-apps/src/test/resources/org/mdpnp/apps/testapp/rbs/ruleset-pea.js
+++ interop-lab/demo-apps/src/test/resources/org/mdpnp/apps/testapp/rbs/ruleset-pea.js
@@ -4,19 +4,11 @@ var State       = org.mdpnp.apps.testapp.vital.VitalModel.State;
 
 var create = function (model) {
 
-    var spo2pr = VitalSign.SpO2PulseRate.addToModel(model);
-    spo2pr.setRequired(true);
-    spo2pr.setModelStateTransitionCondition(State.Alarm);
-    spo2pr.setCriticalLow(20);
-    spo2pr.setWarningLow(20);
-    spo2pr.setWarningHigh(300);
-    spo2pr.setCriticalHigh(300);
-
     var hr = VitalSign.ECGHeartRate.addToModel(model);
     hr.setRequired(true);
     hr.setModelStateTransitionCondition(State.Normal);
     hr.setCriticalLow(25);
-    hr.setWarningLow(40);
+    hr.setWarningLow(50);
     hr.setWarningHigh(150);
     hr.setCriticalHigh(175);
 
@@ -25,24 +17,32 @@ var create = function (model) {
     spo2.setModelStateTransitionCondition(State.Alarm);
     spo2.setCriticalLow(80);
     spo2.setWarningLow(80);
-    spo2.setWarningHigh(101);
-    spo2.setCriticalHigh(101);
+    spo2.setWarningHigh(100);
+    spo2.setCriticalHigh(100);
+
+    var spo2pr = VitalSign.SpO2PulseRate.addToModel(model);
+    spo2pr.setRequired(true);
+    spo2pr.setModelStateTransitionCondition(State.Alarm);
+    spo2pr.setCriticalLow(20);
+    spo2pr.setWarningLow(20);
+    spo2pr.setWarningHigh(225);
+    spo2pr.setCriticalHigh(250);
 
     var ibp = VitalSign.InvSystolic.addToModel(model);
     ibp.setRequired(true);
     ibp.setModelStateTransitionCondition(State.Alarm);
     ibp.setCriticalLow(30);
     ibp.setWarningLow(30);
-    ibp.setWarningHigh(200);
-    ibp.setCriticalHigh(200);
+    ibp.setWarningHigh(225);
+    ibp.setCriticalHigh(250);
 
     var etco2 = VitalSign.EndTidalCO2.addToModel(model);
     etco2.setRequired(true);
     etco2.setModelStateTransitionCondition(State.Alarm);
     etco2.setCriticalLow(15);
     etco2.setWarningLow(15);
-    etco2.setWarningHigh(200);
-    etco2.setCriticalHigh(200);
+    etco2.setWarningHigh(100);
+    etco2.setCriticalHigh(120);
 
     var obj =
     {
