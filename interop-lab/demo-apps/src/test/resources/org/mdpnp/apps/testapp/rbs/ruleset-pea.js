9abe160d6b0c7492dacfd4a95e77af9cd0732f63
j-peterson <j.t.peterson01@gmail.com>
2015-10-21 15:22:19 -0400

    updated vital limits for PEA demo

--- interop-lab/demo-apps/src/test/resources/org/mdpnp/apps/testapp/rbs/ruleset-pea.js
+++ interop-lab/demo-apps/src/test/resources/org/mdpnp/apps/testapp/rbs/ruleset-pea.js
@@ -7,31 +7,31 @@ var create = function (model) {
     var spo2pr = VitalSign.SpO2PulseRate.addToModel(model);
     spo2pr.setRequired(true);
     spo2pr.setModelStateTransitionCondition(State.Alarm);
-    spo2pr.setCriticalLow(25);
-    spo2pr.setWarningLow(30);
-    spo2pr.setWarningHigh(150);
-    spo2pr.setCriticalHigh(175);
+    spo2pr.setCriticalLow(20);
+    spo2pr.setWarningLow(20);
+    spo2pr.setWarningHigh(300);
+    spo2pr.setCriticalHigh(300);
 
     var hr = VitalSign.ECGHeartRate.addToModel(model);
     hr.setRequired(true);
     hr.setModelStateTransitionCondition(State.Normal);
     hr.setCriticalLow(25);
-    hr.setWarningLow(30);
+    hr.setWarningLow(40);
     hr.setWarningHigh(150);
     hr.setCriticalHigh(175);
 
     var spo2 = VitalSign.SpO2.addToModel(model);
     spo2.setRequired(true);
     spo2.setModelStateTransitionCondition(State.Alarm);
-    spo2.setCriticalLow(70);
-    spo2.setWarningLow(75);
+    spo2.setCriticalLow(80);
+    spo2.setWarningLow(80);
     spo2.setWarningHigh(101);
     spo2.setCriticalHigh(101);
 
     var ibp = VitalSign.InvSystolic.addToModel(model);
     ibp.setRequired(true);
     ibp.setModelStateTransitionCondition(State.Alarm);
-    ibp.setCriticalLow(20);
+    ibp.setCriticalLow(30);
     ibp.setWarningLow(30);
     ibp.setWarningHigh(200);
     ibp.setCriticalHigh(200);
@@ -39,8 +39,8 @@ var create = function (model) {
     var etco2 = VitalSign.EndTidalCO2.addToModel(model);
     etco2.setRequired(true);
     etco2.setModelStateTransitionCondition(State.Alarm);
-    etco2.setCriticalLow(20);
-    etco2.setWarningLow(30);
+    etco2.setCriticalLow(15);
+    etco2.setWarningLow(15);
     etco2.setWarningHigh(200);
     etco2.setCriticalHigh(200);
 
