dbc933a5b6c53113ef976fbd0581d3884fc2614f
Dylan Bagshaw <dbagshaw@partners.org>
2018-01-02 13:24:48 -0500

    Revert "Changed how the app displays the infusion percent. Now calculates to 100% instead of 1%. Update #2 to include some missed classes in the first commit."
    This reverts commit 52a3b9fe77d4f23cdfd35a5dfad241436b75c272.

--- interop-lab/demo-devices/src/main/java/org/mdpnp/devices/hospira/symbiq/DemoSymbiq.java
+++ interop-lab/demo-devices/src/main/java/org/mdpnp/devices/hospira/symbiq/DemoSymbiq.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017, MD PnP Program
+ * Copyright (c) 2014, MD PnP Program
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
