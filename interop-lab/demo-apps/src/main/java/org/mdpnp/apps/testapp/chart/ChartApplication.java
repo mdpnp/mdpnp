5e959784fa48a448260a732e88f35d08d900d743
Simon <skelly@agitar.com>
2018-11-19 10:11:33 +0000

    Add Newer/Older/Resume buttons that allow the charts to be scrolled, and also add a date axis per graph

--- interop-lab/demo-apps/src/main/java/org/mdpnp/apps/testapp/chart/ChartApplication.java
+++ interop-lab/demo-apps/src/main/java/org/mdpnp/apps/testapp/chart/ChartApplication.java
@@ -1,6 +1,7 @@
 package org.mdpnp.apps.testapp.chart;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
 
@@ -15,6 +16,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.Pane;
 import javafx.util.Duration;
@@ -25,10 +27,22 @@ import org.mdpnp.apps.testapp.vital.VitalModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * This is the application where users can add vital signs to charts.  The time intervals
+ * available in the selection box are defined here in the enum.  The animation of the chart
+ * is provided by the implementation of EventHandler<ActionEvent>, that is invoked by the
+ * KeyFrame for the Timeline.  Then handle() method calculates the current time and subtracts
+ * the interval, and those two values then set the upper and lower bounds for the x-axis.  As
+ * the KeyFrame has a one second interval, the graph is shifted by 1 second each second.
+ *
+ */
 public class ChartApplication implements ListChangeListener<Vital>, EventHandler<ActionEvent>, ChartApplicationFactory.WithVitalModel {
     protected static final Logger log = LoggerFactory.getLogger(ChartApplication.class);
-    final DateAxis dateAxis = new DateAxis();
+    private ArrayList<DateAxis> dateAxes=new ArrayList<DateAxis>();	//Add to this for each chart...
     @FXML Pane charts;
+    @FXML Button forward;
+    @FXML Button backward;
+    @FXML Button resume;
     VitalModel vitalModel; 
     private Timeline timeline;
     @FXML ComboBox<VitalSign> vitalSigns;
@@ -76,8 +90,7 @@ public class ChartApplication implements ListChangeListener<Vital>, EventHandler
     public void setModel(VitalModel vitalModel) {
         timeInterval.setItems(FXCollections.observableArrayList(TimeInterval.values()));
         vitalSigns.setItems(FXCollections.observableArrayList(VitalSign.values()));
-        dateAxis.setAutoRanging(false);
-        dateAxis.setAnimated(false);
+
         if(null != this.vitalModel) {
             this.vitalModel.removeListener(this);
             if(timeline!=null) {
@@ -120,8 +133,14 @@ public class ChartApplication implements ListChangeListener<Vital>, EventHandler
                 try {
                     Parent node = loader.load();
                     Chart chart = loader.getController();
+                    //New chart, new DateAxis
+                    DateAxis dateAxis=new DateAxis();
+                    dateAxis.setAutoRanging(false);
+                    dateAxis.setAnimated(false);
+                    dateAxes.add(dateAxis);
                     chart.setModel(vi, dateAxis);
                     node.setUserData(chart);
+                    System.err.println("Added chart has x-azis "+chart.lineChart.getXAxis());
                     chart.getRemoveButton().setOnAction(new EventHandler<ActionEvent>() {
 
                         @Override
@@ -130,7 +149,56 @@ public class ChartApplication implements ListChangeListener<Vital>, EventHandler
                         }
                         
                     });
-                    charts.getChildren().add(node);
+                    backward.setOnAction(new EventHandler<ActionEvent>() {
+
+						@Override
+						public void handle(ActionEvent event) {
+							//Pause or stop?
+							timeline.pause();
+							Date currentLower=dateAxis.getLowerBound();
+							Date currentUpper=dateAxis.getUpperBound();
+							long lowerMillis=currentLower.getTime();
+							lowerMillis-=interval;
+							Date newLower=new Date(lowerMillis);
+							long upperMillis=currentUpper.getTime();
+							upperMillis-=interval;
+							Date newUpper=new Date(upperMillis);
+							dateAxes.forEach(a -> {
+								a.setLowerBound(newLower);
+								a.setUpperBound(newUpper);
+							});
+							
+						}
+
+                    });
+                    
+                    forward.setOnAction(new EventHandler<ActionEvent>() {
+
+						@Override
+						public void handle(ActionEvent event) {
+							//Pause or stop?
+							timeline.pause();
+							Date currentLower=dateAxis.getLowerBound();
+							Date currentUpper=dateAxis.getUpperBound();
+							long lowerMillis=currentLower.getTime();
+							lowerMillis+=interval;
+							Date newLower=new Date(lowerMillis);
+							long upperMillis=currentUpper.getTime();
+							upperMillis+=interval;
+							Date newUpper=new Date(upperMillis);
+							dateAxes.forEach(a -> {
+								a.setLowerBound(newLower);
+								a.setUpperBound(newUpper);
+							});
+						}
+						
+                    });
+                    
+                    resume.setOnAction( e -> {
+                    	timeline.play();
+                    });
+                    
+                    charts.getChildren().add(0,node);
                 } catch (IOException e) {
                     log.error("Unable to create a Chart", e);
                 }
@@ -144,8 +212,15 @@ public class ChartApplication implements ListChangeListener<Vital>, EventHandler
     public void handle(ActionEvent event) {
         long now = System.currentTimeMillis();
         now -= now % 1000;
-        dateAxis.setLowerBound(new Date(now - interval));
-        dateAxis.setUpperBound(new Date(now));
+        Date lowerBound=new Date(now - interval);
+        Date upperBound=new Date(now);
+        dateAxes.forEach( a -> {
+        	//a is a DateAxis object
+        	a.setLowerBound(lowerBound);
+            a.setUpperBound(upperBound);	
+        });
+        
+        //System.err.printf("lower and upper now %s and %s\n",lowerBound,upperBound);
     }
 
     @FXML public void addVitalSign() {
