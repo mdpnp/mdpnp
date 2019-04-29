91ecec51470e93040ddfb0d446b07f821c1f1f39
Simon Kelly <35110418+simonkelly1973@users.noreply.github.com>
2019-04-29 15:52:16 +0100

    Issues #13 - Initial load of purejavacomm and build changes to build it

---
+++ interop-lab/purejavacomm/src/purejavacomm/PureJavaComm.java
@@ -0,0 +1,14 @@
+package purejavacomm;
+
+public class PureJavaComm {
+	// change this if you modify the code and re-distribute
+	public static String getVersion() {
+		return "1.0.3";
+	}
+
+	// change this is if you fork the project
+	public static String getFork() {
+		return "SpareTimeLabs";
+	}
+
+}
